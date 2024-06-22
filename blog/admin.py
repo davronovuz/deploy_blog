fa0986014a445777561c1d2c2b9f5dc55888fe2f
davronovtatu <davronovtatu@gmail.com>
2024-06-22 23:05:10 +0500

    too version

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Blog,Contact
 
-# Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["title","created_at"]
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["full_name","phone_number","created_at"]
+
+
+
+admin.site.register(Blog,BlogAdmin)
+admin.site.register(Contact,ContactAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Blog(models.Model):
+    title=models.CharField(max_length=250)
+    body=models.TextField()
+    image=models.CharField(max_length=500)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=17)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
